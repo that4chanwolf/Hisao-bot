e993c25cf4d2e19b5dbebf33ee68e3e2f4487f77
Wise Horo <that4chanwolf@gmail.com>
2012-10-31 07:01:07 -0400

    waa.ai shortening was too inefficient

--- bot.js
+++ bot.js
@@ -82,18 +82,7 @@ app.post('/', function(req, res) {
 	rc.channels.forEach(function(channel) {
 		client.say(channel, irc.colors.codes.light_gray + payload.repository.name + irc.colors.codes.reset + " (" + irc.colors.codes.light_blue + payload.repository.language + irc.colors.codes.reset + ") had " + payload.commits.length + " commit" + ( payload.commits.length > 1 ? "s" : "" ) + " added.");
 		payload.commits.forEach(function(commit) {
-			if(!urls[commit.url]) {
-				request.get("http://waa.ai/api.php?url=" + commit.url,function (error, response, body) {
-					if(error || !response || !body || body.trim() === "") {
-						client.say(channel, irc.colors.codes.light_magenta + "[" + commit.author.name + "]" + irc.colors.codes.reset + " " + commit.message.replace(/\n/gim, " ") + " " + irc.colors.codes.light_green + "[" + payload.ref.split('/')[payload.ref.split("/").length-1] + "]" + irc.colors.codes.reset);
-					} else {
-						client.say(channel, irc.colors.codes.light_magenta + "[" + commit.author.name + "]" + irc.colors.codes.reset + " " + commit.message.replace(/\n/gim, " ") + " " + irc.colors.codes.light_green + "[" + payload.ref.split('/')[payload.ref.split("/").length-1] + "]" + irc.colors.codes.reset + " " + irc.colors.codes.light_blue + body.trim());
-						urls[commit.url] = body.trim();
-					}
-				});
-			} else {
-				client.say(channel, irc.colors.codes.light_magenta + "[" + commit.author.name + "]" + irc.colors.codes.reset + " " + commit.message.replace("\n", ", ") + " " + irc.colors.codes.light_green + "[" + payload.ref.split('/')[payload.ref.split("/").length-1] + "]" + irc.colors.codes.reset + " " + irc.colors.codes.light_blue + urls[commit.url]);
-			}
+			client.say(channel, irc.colors.codes.light_magenta + "[" + commit.author.name + "]" + irc.colors.codes.reset + " " + commit.message.replace(/\n/gim, " ") + " " + irc.colors.codes.light_green + "[" + payload.ref.split('/')[payload.ref.split("/").length-1] + "]" + irc.colors.codes.reset + " " + irc.colors.codes.light_blue + commit.url);
 		});
 	});
 });
